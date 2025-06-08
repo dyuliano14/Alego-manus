b97bd5c087b228874cbb948cda5d4bf963e83c7b
Dyuliano Magalhães <dyuliano.magalhaes@academico.ifg.edu.br>
2025-06-08 13:52:25 +0000

    main1

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./src/index.css"; // se usar Tailwind
+import "./styles/css/index.css"; // se usar Tailwind
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
