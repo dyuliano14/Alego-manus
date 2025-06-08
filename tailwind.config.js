c26965cb6d26fb24c90d4cbaeca98803e4d2e701
Dyuliano Magalhães <dyuliano.magalhaes@academico.ifg.edu.br>
2025-06-08 12:55:52 +0000

    src-corrigido

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class', // ou 'media'
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx,md}',
-  ],
+  darkMode: "class", // ou 'media'
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,md}"],
   theme: { extend: {} },
-  plugins: []
+  plugins: [require("@tailwindcss/typography")],
 };
