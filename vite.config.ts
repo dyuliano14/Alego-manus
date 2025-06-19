b4de3ec9e9cb8b2829e85a6d6df0582de2d9288a
Dyuliano Magalhães <dyuliano.magalhaes@academico.ifg.edu.br>
2025-06-19 11:27:05 +0000

    impa e adjust

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { fileURLToPath } from "url"; // Necessário para __dirname em ESM
+import { fileURLToPath } from "url"; // ESSENCIAL para __dirname em ESM
 
-// Define __dirname e __filename para o contexto de Módulos ES
+// Obtém o caminho do arquivo atual e o diretório, compatível com ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -12,10 +12,10 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"), // Certifique-se que o caminho esteja correto aqui
+      "@": path.resolve(__dirname, "src"), // Garante que o '@' aponte para a pasta 'src'
     },
   },
   server: {
-    fs: { allow: ["."] }, // Permite o acesso a arquivos fora da raiz, se necessário
+    fs: { allow: ["."] }, // Permite ao servidor Vite acessar arquivos fora da raiz do projeto, se necessário.
   },
 });
