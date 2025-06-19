7cf578aab951fd4f2e2d180a6f3267120c87400e
Dyuliano Magalhães <dyuliano.magalhaes@academico.ifg.edu.br>
2025-06-19 11:08:23 +0000

    adjust5432

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url"; // Importe fileURLToPath do módulo 'url'
+import path from "path"; // Importe 'path'
 
+// Define __dirname em um ambiente de módulo ES
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [
-    react(),
-
-  ],
+  plugins: [react()],
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "src") },
+  },
+  server: {
+    fs: { allow: ["."] },
   },
 });
