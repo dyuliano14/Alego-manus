2385b6f047b1a61d4292aa108c2503f2cd09b126
Dyuliano Magalhães <dyuliano.magalhaes@academico.ifg.edu.br>
2025-06-19 10:54:30 +0000

    boraver

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,6 @@
+// postcss.config.js
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {},
+    tailwindcss: {}, // <-- Corrigido aqui para ser apenas 'tailwindcss'
     autoprefixer: {},
   },
-};
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwind from "@tailwindcss/vite"; // import ESM
 
 import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwind(), // 👈 agora funciona corretamente
-  ],
+  plugins: [react()],
   resolve: {
     alias: { "@": path.resolve(__dirname, "src") },
   },
