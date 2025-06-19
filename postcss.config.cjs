8c47363772d9332aaacb8450012708f5e42125b6
Dyuliano Magalhães <dyuliano.magalhaes@academico.ifg.edu.br>
2025-06-19 11:54:48 +0000

    tome-lhe

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,10 @@
 // postcss.config.js
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    // Use @tailwindcss/postcss como sugerido pelo Netlify
+    // Isso é uma solução temporária para o erro específico do Netlify
+    // e pode ser revertido se o problema for corrigido em versões futuras ou no Netlify
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
-}; // <-- As chaves de fechamento são importantes!
+};
