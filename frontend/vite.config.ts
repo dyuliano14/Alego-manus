f8d11d33ac53f8c17741ce7eca88077f31ad5f60
Dyuliano Magalhães <dyuliano.magalhaes@academico.ifg.edu.br>
2025-07-13 01:33:04 +0000

    Analise

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     watch: {
       ignored: ["**/backend/**"],
     },
-    allowedHosts: [".replit.dev", "localhost"],
+    allowedHosts: ["origin: true"],
   },
 
   optimizeDeps: {
